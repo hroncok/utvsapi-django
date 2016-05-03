10cd2a830aa2d1f2062b1856856afbb19adb753f
Miro Hrončok <miro@hroncok.cz>
2016-05-03 19:34:18 +0200

    Quotes

--- api/filters.py
+++ api/filters.py
@@ -2,10 +2,10 @@ from rest_framework import filters
 
 
 class ObjectPermissionsFilter(filters.BaseFilterBackend):
-    """
+    '''
     A filter backend that limits results to those where the requesting user
     has read object level permissions.
-    """
+    '''
     def filter_queryset(self, request, queryset, view):
         if request.auth['_everything']:
             return queryset
