c13a43f89c6eb49281364153a0626c4b91358477
Miro Hrončok <miro@hroncok.cz>
2016-05-03 18:29:36 +0200

    Be sonsistent with quotes

--- api/permissions.py
+++ api/permissions.py
@@ -2,9 +2,9 @@ from rest_framework import permissions, exceptions
 
 
 class HasGeneralReadScope(permissions.BasePermission):
-    """
+    '''
     Allows access only to clients with general scope.
-    """
+    '''
 
     def has_permission(self, request, view):
         return (
